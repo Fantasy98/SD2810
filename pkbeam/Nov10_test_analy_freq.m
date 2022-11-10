9ea47e4b44401cc115426f90fab3f6be05ee9710
yuningwang <wangyuningie123@163.com>
2022-11-10 14:50:59 +0100

    Examine the analytical bending frequencies

--- pkbeam/Nov10_test_analy_freq.m
+++ pkbeam/Nov10_test_analy_freq.m
@@ -1,10 +1,6 @@
 
-% lwmain.m
-%
-% main program for aeroelastic analysis
-%
-% (c) 2004-2016 Dan Borglund <dodde@kth.se> and David Eller <dlr@kth.se>
-
+% For test the frequency with analytical eigenfrequncy
+% Clamped Beam ! 
 clear all;
 
 % setup geometry and structural properties
@@ -16,22 +12,24 @@ nnodes = nelem + 1;
 % lab wing dimensions and properties
 l =1.6; % m
 b = 0.175; % m
-ba = 0.03; % m
+% Here we do not consider the aileron 
+%and mass of hinge since the test will be apply on the flat plate
 ba = 0;
-% measured from lab
-mhinge = (40.3 + 38.78 + 20.06 + 6.39 + 28.92 + 20.06 + 10.39 + 4.08 + 5.57)/1000; % kg
 mhinge = 0;
 t = 0.003;%m
+% The estimate data from David
+% which should be adjust after experiment !
+%################################
 rhop = 2000; % m^3/kg
 rhop = 1900;
 % A guess of E and G 
 E = 25E9;
 % Test Result
-E = 20E9; % Gpa
+E = 22E9; % Gpa
 % Assumed Possion Ratio
 possion = 0.3;
 G = E/2*(1+possion);
-%G = E/2;
+%################################
 
 % definition matrix for discrete point masses to attach
 npmass = 0 ;
@@ -41,7 +39,7 @@ dpm(2,:) =0;
 % ....
 
 % set up linear constraints for clamped wing root
-%% Number of Degree of freedom
+% Number of Degree of freedom
 ndof = 3*nnodes;
 B = eye(3,ndof);
 % Here we are testing condition without clamping
@@ -57,22 +55,18 @@ I= (2*b*t^3)/12
 % print free vibration frequencies
 [V,LAMBDA] = eig(K,M);
 Vhat = Z * V;
-omega = sqrt(Z*LAMBDA)./(2*pi);
-% fprintf("Eigen Frequencies are %.2f rad/s \n",omega(1:5,1:5));
+omega = diag(sqrt(LAMBDA)./(2*pi));
+fprintf("First Three bending frequencies from eigenvalue is %.2f rad/s \n",[omega(1) omega(2) omega(4)])
+% Theoritical Formula for frequncies
 
+fprintf("Analytical Solution of Frequencies: \n")
+% 1. Bending frequency: 
 % Compute beam mass per length
 my = rhop * 2*b*t;
 % Constant Bn values for  3 lowest modes for free
 Bn = [3.52 22.0 61.7];
-
 f_bending = Bn.*sqrt(E*I/my)./(2*pi*l^2);
-
 fprintf("Frequency of bending is %.2f rad/s \n", f_bending );
 
-% show modeshapes
-%% Plot the bending mode
-vismode(Vhat(:,1))
-
-% stop here until the rest is implemented
 return;
 
--- pkbeam/Nov11_exp_freq.m
+++ pkbeam/Nov11_exp_freq.m
@@ -1,37 +1,35 @@
 
-% lwmain.m
-%
-% main program for aeroelastic analysis
-%
-% (c) 2004-2016 Dan Borglund <dodde@kth.se> and David Eller <dlr@kth.se>
-
+% For test the frequency with analytical eigenfrequncy
+% Free End Beam
 clear all;
 
 % setup geometry and structural properties
 % number of finite elements requested should be a multiple of 3
-nelem = 12;
+nelem = 20;
 
 nnodes = nelem + 1;
 
 % lab wing dimensions and properties
 l =1.6; % m
 b = 0.175; % m
-ba = 0.03; % m
+% Here we do not consider the aileron 
+%and mass of hinge since the test will be apply on the flat plate
 ba = 0;
-% measured from lab
-mhinge = (40.3 + 38.78 + 20.06 + 6.39 + 28.92 + 20.06 + 10.39 + 4.08 + 5.57)/1000; % kg
 mhinge = 0;
 t = 0.003;%m
+% The estimate data from David
+% which should be adjust after experiment !
+%################################
 rhop = 2000; % m^3/kg
 rhop = 1900;
 % A guess of E and G 
 E = 25E9;
 % Test Result
-E = 20E9; % Gpa
+E = 22E9; % Gpa
 % Assumed Possion Ratio
 possion = 0.3;
 G = E/2*(1+possion);
-%G = E/2;
+%################################
 
 % definition matrix for discrete point masses to attach
 npmass = 0 ;
@@ -41,9 +39,11 @@ dpm(2,:) =0;
 % ....
 
 % set up linear constraints for clamped wing root
-%% Number of Degree of freedom
+% Number of Degree of freedom
 ndof = 3*nnodes;
 B = eye(3,ndof);
+% No constrain == Free 
+B = [];
 % Here we are testing condition without clamping
 
 % retrieve system matrices
@@ -57,22 +57,23 @@ I= (2*b*t^3)/12
 % print free vibration frequencies
 [V,LAMBDA] = eig(K,M);
 Vhat = Z * V;
-omega = sqrt(Z*LAMBDA)./(2*pi);
-% fprintf("Eigen Frequencies are %.2f rad/s \n",omega(1:5,1:5));
+omega = diag(sqrt(LAMBDA)./(2*pi));
+fprintf("First Three bending frequencies from eigenvalue is %.2f rad/s \n",omega(1:4:16))
+fprintf("First Three bending frequencies from eigenvalue is %.2f rad/s \n",omega(2:4:16))
+fprintf("First Three bending frequencies from eigenvalue is %.2f rad/s \n",omega(4:4:16))
+% Theoritical Formula for frequncies
 
+fprintf("Analytical Solution of Frequencies: \n")
+% 1. Bending frequency: 
 % Compute beam mass per length
 my = rhop * 2*b*t;
 % Constant Bn values for  3 lowest modes for free
-Bn = [3.52 22.0 61.7];
-
-f_bending = Bn.*sqrt(E*I/my)./(2*pi*l^2);
-
+uL = [0 4.730 7.853];
+r = 3:10;
+url = (2.*r+1/2).*pi;
+UL = [uL url];
+f_bending = ((UL).^2)*sqrt(E*I/(my*l^4));
 fprintf("Frequency of bending is %.2f rad/s \n", f_bending );
 
-% show modeshapes
-%% Plot the bending mode
-vismode(Vhat(:,1))
-
-% stop here until the rest is implemented
 return;
 
